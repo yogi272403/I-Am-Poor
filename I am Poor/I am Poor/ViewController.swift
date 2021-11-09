aa60b0318be0e5133e790da131b3b898b07663bf
Yogi N. Patel <93512321+yogi272403@users.noreply.github.com>
2021-11-09 09:53:51 +0530

    Add files via upload

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Yogi Patel on 03/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
